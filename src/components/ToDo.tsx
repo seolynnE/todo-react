a27e2de62db62a75504021912fd758db29c9ec6e
seolynnE <tjfls2004@gmail.com>
2024-01-13 02:37:57 +0900

    btn 기능 추가

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -28,6 +28,15 @@ function ToDo({ text, category, id }: IToDo) {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
 
   return (
